8ffd42860ff5a6b31f5da575a26c39cb654d0158
keeping <starunaway@gmail.com>
2020-08-11 19:35:14 +0800

    feat: 🎸 humburgerMenu

--- src/pages/humburgerMenu/index.js
+++ src/pages/humburgerMenu/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import './style.less';
 
 export default () => {
-  return <div>menu</div>;
+  return (
+    <div className='menu'>
+      <input type='checkbox' id='toggler' />
+      <label htmlFor='toggler'>
+        <div className='menu-icon'>
+          <span className='menu-icon-line-1' />
+          <span className='menu-icon-line-2' />
+        </div>
+      </label>
+      <div className='menu-list'>
+        {'123456'.split('').map((v) => (
+          <li key={v}>
+            {v}
+            -items
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 };
