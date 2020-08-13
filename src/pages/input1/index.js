0ce2c920bf55f4115e80fd2ae24bddea8bd5af19
keeping <starunaway@gmail.com>
2020-08-13 23:31:29 +0800

    feat: 🎸 login1

--- src/pages/input1/index.js
+++ src/pages/input1/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './style.less';
 
-export default () => (
-  <div className='login-html'>
-    <h5>账号登录</h5>
-    <div className='input-wrapper'>
-      <input type='text' id='username' autoComplete='off' placeholder=' ' />
-      <label htmlFor='username'>用户名</label>
-    </div>
-    <div className='input-wrapper'>
-      <input type='text' id='password' autoComplete='off' placeholder=' ' />
-      <label htmlFor='password'>密码</label>
-    </div>
-  </div>
-);
+export default () => [
+  <h5 key={1}>账号登录</h5>,
+  <div className='input-wrapper' key={2}>
+    <input type='text' id='username' autoComplete='off' placeholder=' ' />
+    <label htmlFor='username'>用户名</label>
+  </div>,
+  <div className='input-wrapper' key={3}>
+    <input type='text' id='password' autoComplete='off' placeholder=' ' />
+    <label htmlFor='password'>密码</label>
+  </div>,
+];
