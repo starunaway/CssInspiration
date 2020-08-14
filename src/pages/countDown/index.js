a3e1f7a57c72e34abc49b0436bb9479b4b07a1fa
keeping <starunaway@gmail.com>
2020-08-14 18:15:09 +0800

    refactor: 💡 countDown hook

--- src/pages/countDown/index.js
+++ src/pages/countDown/index.js
@@ -3,48 +3,70 @@ import './style.less';
 
 export default () => {
   const circleRef = useRef();
+  const intervalRef = useRef();
+
+  const [percent, setPercent] = useState(0);
+  const [strokeDashoffset, setStrokeDashoffset] = useState(0);
+  const [isStart, setIsStart] = useState(false);
 
   const handleStart = () => {
-    console.log(this.timer);
-    this.timer = 100;
-    if (!this.timer) {
-      this.timer = setInterval(() => {
-        const {percent} = this.state;
-        console.log(percent);
-        if (percent >= 100) {
-          console.log('end', percent);
-          this.setState({percent: 0, strokeDashoffset: 0}, () => {
-            const a = this.state;
-            console.log('reset', a.percent);
-          });
-
-          clearInterval(this.timer);
-          this.timer = null;
-        } else {
-          this.setState({
-            percent: percent + 0.1,
-            strokeDashoffset: (Math.PI * 2 * 55 * percent) / 100,
-          });
-        }
-      }, 5);
+    if (!isStart) {
+      setIsStart(true);
     }
-
-    this.circle.classList = [];
-    setTimeout(() => {
-      this.circle.classList = ['start'];
-    });
   };
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      console.log('percent', percent);
+      if (percent >= 100) {
+        setPercent(0);
+        setStrokeDashoffset(0);
+        clearInterval(timer);
+        timer = null;
+        setIsStart(false);
+      } else {
+        setPercent((state) => state + 0.1);
+        setStrokeDashoffset((Math.PI * 2 * 55 * percent) / 100);
+      }
+    }, 3);
+
+    intervalRef.current = timer;
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [null, isStart]);
+
+  //   const handleStart = () => {
+  //     // console.log(timer);
+  //     // if (!timer) {
+  //     //   timer = setInterval(() => {
+  //     //     if (percent >= 100) {
+  //     //       setPercent(0);
+  //     //       setStrokeDashoffset(0);
+  //     //       clearInterval(timer);
+  //     //       timer = null;
+  //     //     } else {
+  //     //       setPercent((state) => state + 0.1);
+  //     //       setStrokeDashoffset((Math.PI * 2 * 55 * percent) / 100);
+  //     //     }
+  //     //   }, 5);
+  //     // }
+
+  //     this.circle.classList = [];
+  //     setTimeout(() => {
+  //       this.circle.classList = ['start'];
+  //     });
+  //   };
+
+  console.log('render--', percent, strokeDashoffset);
 
   return (
     <div className='cicle-container'>
       <div className='circle-wrapper'>
-        <svg
-          width={120}
-          height={120}
-          ref={(ref) => {
-            this.circle = ref;
-          }}
-        >
+        <svg width={120} height={120} ref={circleRef}>
           <circle fill='transparent' r={55} cx={60} cy={60} strokeWidth={5} stroke='red' />
           <circle
             id='circle'
@@ -56,7 +78,7 @@ export default () => {
             strokeLinecap='round'
             stroke='#303131'
             strokeDasharray={Math.PI * 2 * 55}
-            strokeDashoffset={0}
+            strokeDashoffset={strokeDashoffset}
           />
         </svg>
         <span className='time' />
