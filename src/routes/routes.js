29e202888459198770d4b4d45e70fc0d5de7e0e9
keeping <starunaway@gmail.com>
2020-08-25 17:41:20 +0800

    feat: 🎸 add react-loadable, 在对应路由下加载对应的样式

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,59 +1,77 @@
-import HumburgerMenu from '@pages/humburgerMenu';
-import Home from '@pages/home';
-import flippingCard from '@pages/flippingCard';
-import Input1 from '@pages/input1';
-import CountDown from '@pages/countDown';
-import Grid from '@pages/cssGrid';
-import SpotLight from '@pages/spotLight';
-import SuccessTick from '@pages/successTick';
+import Loadable from 'react-loadable';
+import React from 'react';
 
 const ROUTES = [
   {
     description: '首页',
     path: '/home',
-    component: Home,
+    component: Loadable({
+      loader: () => import('@pages/home'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: [],
   },
   {
     description: '打勾',
     path: '/successTick',
-    component: SuccessTick,
+    component: Loadable({
+      loader: () => import('@pages/successTick'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['打勾'],
   },
   {
     description: '聚光灯',
     path: '/spotLight',
-    component: SpotLight,
+    component: Loadable({
+      loader: () => import('@pages/spotLight'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['聚光灯', 'spotlight'],
   },
   {
     description: 'Grid布局',
     path: '/grid',
-    component: Grid,
+    component: Loadable({
+      loader: () => import('@pages/cssGrid'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['Grid布局', 'Grid'],
   },
   {
     description: '倒计时',
     path: '/countDown',
-    component: CountDown,
+    component: Loadable({
+      loader: () => import('@pages/countDown'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['倒计时'],
   },
   {
     description: '菜单',
     path: '/humburgerMenu',
-    component: HumburgerMenu,
+    component: Loadable({
+      loader: () => import('@pages/humburgerMenu'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['菜单', 'menu', 'Menu', 'MENU'],
   },
   {
     description: '翻牌子',
     path: '/flippingCard',
-    component: flippingCard,
+    component: Loadable({
+      loader: () => import('@pages/flippingCard'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['翻转', 'cover', '卡片', 'card'],
   },
   {
     description: 'Input动态Label',
     path: '/input1',
-    component: Input1,
+    component: Loadable({
+      loader: () => import('@pages/input1'),
+      loading: () => <span>Loading...</span>,
+    }),
     keywords: ['Input', '输入', 'Form', 'input'],
   },
 ];
