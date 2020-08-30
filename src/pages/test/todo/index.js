dee0ce0e3901e9281a45ec66df873cbdba2a6eaa
keeping <starunaway@gmail.com>
2020-08-30 22:33:58 +0800

    docs: ✏️ todo

--- src/pages/test/todo/index.js
+++ src/pages/test/todo/index.js
@@ -9,6 +9,10 @@ export default () => {
       <li>看一下vue的全教程</li>
       <li>补觉</li>
       <li>源码解析，高级前端课程</li>
+      <li>node相关的知识</li>
+      <li>后端服务相关</li>
+      <li>nginx配置</li>
+      <li>bash脚本的编写</li>
     </ul>
   );
 };
