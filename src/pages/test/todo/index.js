17bef675202c5ef2ef8e9d5ee8e9b77bd7610f50
keeping <starunaway@gmail.com>
2020-08-31 23:23:52 +0800

    docs: ✏️ vue slot

--- src/pages/test/todo/index.js
+++ src/pages/test/todo/index.js
@@ -6,7 +6,10 @@ export default () => {
       <li>学习css</li>
       <li>学习hook</li>
       <li>阅读待读的几篇文章</li>
-      <li>看一下vue的全教程</li>
+      <li>
+        看一下vue的全教程
+        <p>slot：形象的例子：配置一台电脑，需要各种配件。slot：具体的配件，插到插槽里</p>
+      </li>
       <li>补觉</li>
       <li>源码解析，高级前端课程</li>
       <li>node相关的知识</li>
